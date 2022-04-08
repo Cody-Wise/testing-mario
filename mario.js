f78a036241301f87bf3cb453a8f990d053d37acc
Cody-Wise <cody@sharptackmedia.com>
2022-04-07 20:31:00 -0700

    Got castle flush against bricks

--- mario.js
+++ mario.js
@@ -1,14 +1,16 @@
+ 
 import kaboom from './kaboom/dist/kaboom.mjs';
 
 
 kaboom({
     global: true,
+    // width: 600,
+    // height: 300,
     fullscreen: true,
     scale: 2, 
     debug: true,
     background: [0, 0, 0, 1],
 });
-
 
 //sprites, blocks to build world
 loadSprite('coin', 'assets/coin.png');
@@ -19,101 +21,80 @@ loadSprite('mushroom', 'assets/mushroom.png');
 loadSprite('evil-mushroom', 'assets/evil-mushroom.png');
 loadSprite('surprise-box', 'assets/surprise-box.png');
 loadSprite('pipe', 'assets/pipe.png');
+loadSprite('castle', 'assets/castle.png');
+
 
 //sounds to play during gameplay
 // loadRoot('https://dazzling-vacherin-8cb912.netlify.app/assets/');
-loadRoot('/assets/');
-loadSound('jump', 'marioJump.mp3');
-loadSound('theme', 'mainTheme.mp3');
-loadSprite("start-screen", "/start-screen.png");
+// loadRoot('http://localhost:5501/assets/');
+// loadSound('jump', 'marioJump.mp3');
+// loadSound('theme', 'mainTheme.mp3');
 
-
+loadSprite('start-screen', './assets/start-screen.png');
 
 scene('start', () => {
     
+    const startScreen = add([
+        sprite('start-screen'),
+        origin('center'), 
+        pos(0, 0), 
+        scale(0.65)
+    ]);
     add([
-        sprite("start-screen"),origin('center'), pos(475, 150), scale(0.65)
-    ])
-    add([
-        text("Press Spacebar To Start"),origin('center'), pos(475, 275), scale(0.25)
-    ])
-    
+        text('Press Spacebar To Start'),
+        origin('center'), 
+        pos(0, 125), 
+        scale(0.25)
+    ]);
 
     onKeyDown('space', () => {
-        go('game', { score: 0, count: 0 });
-
-        // play('theme');
+        go('game');
     });
     
-
+    startScreen.onUpdate(() => {
+        camPos(startScreen.pos.x, (startScreen.pos.y + 50));
+    });
 });
 
-
-
-
-scene('game', ({ score, count, time }) => {
-
-    // onUpdate(()=> {
-    //     setTimeout(countdown, 1000);
-    // })
-
-//     let timeLeft = 300;
-// function countdown() {
-    
-//     timeLeft--;
-//     if (timeLeft > 0) {
-//         setTimeout(countdown, 1000);
-//     }
-// }
-    
-    
+scene('game', () => {
     layers(['bg', 'obj', 'ui'], 'obj');
-
-    //create maps for the world
-    const map = [
-        '                                                                           ',
-        '                                           %%%%                            ',
-        '                                                                           ',
-        '                                                          ===              ',
-        '                                                                           ',
-        '     *   ===%=                          %===%%==*=             %%%         ',
-        '                                  ===                   =                  ',
-        '                                                        =                  ',
-        '        *           ^   ^                             ^ =                ? ',
-        '==============================   ========================    ==============',
-    ];
-
-    //configuring the map to display
-    const levelConfig = {
-        width: 20,
-        height: 20,
-        '=': () => [sprite('brick'), solid(), area()],
-        '*': () => [sprite('coin'), area(), 'coin'],
-        '%': () => [sprite('surprise-box'), solid(), area(), 'coin-surprise'],
-        '#': () => [sprite('surprise-box'), solid(), area(), 'mushroom-surprise'],
-        '^': () => [sprite('evil-mushroom'), solid(), area(), 'evil-mushroom', body()],
-        '?': () => [sprite('pipe'), solid(), area()],
-        '+': () => [sprite('block'), solid(), area()],
-        '@': () => [sprite('mushroom'), solid(), area(), 'mushroom', body()],
-    };
-
-    const gameLevel = addLevel(map, levelConfig);
 
     const mario = add([
         sprite('mario'), 
         solid(), 
         area(),
-        pos(30, 0),
+        pos(860, 0),
         body(),
         origin('bot'),
         'mario'
     ]);
 
-    //moving mario
+    add([
+        sprite("castle"),
+        pos(1018, 286),
+        layer("bg"),
+        origin("bot"),
+        scale(0.25)
+      ])
+
+    //   mario.onCollide("castle", (obj) => {
+    //     add([
+
+    // if (mario.pos === (860, 280)) {
+    //     add([ text("Well Done!", { size: 24 }),
+    //       pos(toWorld(vec2(160, 120))),
+    //       color(255, 255, 255),
+    //       origin("center"),
+    //       layer('ui'),
+    //     ])}
+    //   console.log(mario.pos);
+
+
     const marioSpeed = 120;
     const marioJumpHeight = 600;
-    const coinScore = 200;
-    
+    const mushroomMove = 40;
+    const evilMushroomMove = 20;
+
 
     onKeyDown('left', () => {
         mario.move(-marioSpeed, 0);
@@ -126,94 +107,114 @@ scene('game', ({ score, count, time }) => {
     onKeyPress('space', () => {
         if (mario.isGrounded()) {
             mario.jump(marioJumpHeight);
-            play('jump');
         }
     });
 
-    //mario actions
-    mario.onCollide('coin', (obj) => {
-        destroy(obj);
-        scoreLabel.value += coinScore;
-        scoreLabel.text = scoreLabel.value;
-        console.log(mario.isGrounded());
-    });
-
-    on('mario', 'coin-surprise', (box) => {
-
-    });
-
-    // mario.onCollide('coin-surprise', (obj) => {
-    //     console.log(isBottom());
-    //     isBottom:()=>35.60<0
-    //     gameLevel.spawn('*', obj.gridPos.sub(0, 1));
-    //     gameLevel.spawn('+', obj.gridPos.sub(0, 0));
-    //     destroy(obj);
-    // });
-
-    mario.on('headbutt', (obj) => {
-        if (obj.is('coin-surprise')) {
+    mario.onCollide('coin-surprise', (obj) => {
+        if (mario.pos.y === obj.pos.y + 40) {
+            destroy(obj);
             gameLevel.spawn('*', obj.gridPos.sub(0, 1));
             gameLevel.spawn('+', obj.gridPos.sub(0, 0));
-            destroy(obj);
         }
-        if (obj.is('mushroom-surprise')) {
+    });
+
+    mario.onCollide('mushroom-surprise', (obj) => {
+        if (mario.pos.y === obj.pos.y + 40) {
+            destroy(obj);
             gameLevel.spawn('@', obj.gridPos.sub(0, 1));
             gameLevel.spawn('+', obj.gridPos.sub(0, 0));
+            onUpdate('mushroom', (obj) => {
+                obj.move(mushroomMove, 0);
+            });
+        }
+    });
+
+    mario.onCollide('brick', (obj) => {
+        if (mario.pos.y === obj.pos.y + 40) {
+            const mushroomSurprises = get('mushroom-surprise');
+            const coinSurprises = get('coin-surprise');
+            for (let mushroomSurprise of mushroomSurprises) {
+                const marioDistance = mushroomSurprise.pos.x - mario.pos.x;
+                if (mario.pos.y === mushroomSurprise.pos.y + 40 && marioDistance > -20 && marioDistance < 0) {
+                    destroy(mushroomSurprise);
+                    gameLevel.spawn('@', mushroomSurprise.gridPos.sub(0, 1));
+                    gameLevel.spawn('+', mushroomSurprise.gridPos.sub(0, 0));
+                    onUpdate('mushroom', (obj) => {
+                        obj.move(mushroomMove, 0);
+                    });
+                }
+            }
+            for (let coinSurprise of coinSurprises) {
+                const marioDistance = coinSurprise.pos.x - mario.pos.x;
+                if (mario.pos.y === coinSurprise.pos.y + 40 && marioDistance > -20 && marioDistance < 0) {
+                    destroy(coinSurprise);
+                    gameLevel.spawn('*', coinSurprise.gridPos.sub(0, 1));
+                    gameLevel.spawn('+', coinSurprise.gridPos.sub(0, 0));
+                }
+            }
+        }
+    });
+
+    mario.onCollide('coin', (obj) => {
+        destroy(obj);
+    });
+
+    onUpdate('evil-mushroom', (obj) => {
+        obj.move(-evilMushroomMove, 0);
+    });
+
+    mario.onCollide('mushroom', (obj) => {
+        destroy(obj);
+    });
+
+    mario.onCollide('evil-mushroom', (obj) => {
+        if (mario.pos.y === obj.pos.y) {
             destroy(obj);
+        } else {
+            go('start');
         }
     });
 
-    //moving the mushrooms both evil and powerups
-    const mushroomMove = 20;
-    action('mushroom', (e) => {
-        e.move(mushroomMove, 0);
+    const gameLevel = addLevel([
+        '                                                           ',
+		'                                                           ',
+        '        ***                                                ',
+        '                                                           ',
+        '                                                           ',
+        '                 ****                                      ',
+        '                                                           ',
+        '                                                           ',
+        '                 ====                                      ',
+        '                                                           ',
+        '                                                           ',
+        '     **   =$=#=                                            ',
+        '                                                           ',
+        '                         ?                                 ',
+        '                    ^  ^                    ^  ^       C   ',
+        '===========================    ========   === ============',
+                                                                 
+    ], {
+        // define the size of each block
+        width: 20,
+        height: 20,
+        pos: vec2(0, -20),
+        // define what each symbol means, by a function returning a component list (what will be passed to add())
+        '=': () => [sprite('brick'), area(), solid(), 'brick'],
+        '*': () => [sprite('coin'), area(), 'coin'],
+        '$': () => [sprite('surprise-box'), solid(), area(), 'coin-surprise'],
+        '#': () => [sprite('surprise-box'), solid(), area(), 'mushroom-surprise'],
+        '^': () => [sprite('evil-mushroom'), solid(), area(), 'evil-mushroom', body()],
+        '?': () => [sprite('pipe'), solid(), area()], 
+        '+': () => [sprite('block'), solid(), area()],
+        '@': () => [sprite('mushroom'), solid(), area(), 'mushroom', body()],
+        // 'C': () => [sprite('castle'), area(), origin('bot'),scale(0.25), 'castle']
     });
 
-    action('evil-mushroom', (e) => {
-        e.move(-mushroomMove, 0);
-    });
-
-    // mario.collides('mushroom', (e) => {
-    //     destroy(e);
-    //     mario.biggify(10);
-    // });
-
-
-
-
-    // play('theme');
-
-    const username = add([
-        text('MARIO', {
-            size: 18,
-            width: 320, 
-            font: 'sinko', 
-        }),
-        pos(30, 6),
-        fixed()
-    ]);
-
-     
-
-    console.log(username);
-    const scoreLabel = add([
-        text(score, {
-            size: 18,
-            width: 320, 
-            font: 'sinko', 
-        }),
-        pos(60, 30),
-        layer('ui'),
-        fixed(),
-        {
-            value: score
-        }
-    ]);
-    let timeLeft = 10
+    let timeLeft = 6000;
 
     const timer = add([
         
-        text(timeLeft, {
+        text(timeLeft / 60, {
             size: 18,
             width: 320, 
             font: 'sinko', 
@@ -223,47 +224,37 @@ scene('game', ({ score, count, time }) => {
         fixed(),
         {
             value: time
-        }
+        },
+        'timer'        
+    ]);
 
+    onUpdate('timer', (obj) => {
+        timeLeft--; 
+        if ((timeLeft / 60) % 1 === 0) {
+            destroy(obj);
+            const timer = add([
         
-    ]);
-
-    add([sprite('coin'), pos(200, 32), layer('ui'), fixed()]);
-    
-    const coinCount = add([
-        text('x' + count, {
-            size: 18,
-            width: 320, 
-            font: 'sinko', 
-        }),
-        pos(220, 30),
-        fixed(),
-        layer('ui'),
-        {
-            value: count
+                text(timeLeft / 60, {
+                    size: 18,
+                    width: 320, 
+                    font: 'sinko', 
+                }),
+                pos(80, 30),
+                layer('ui'),
+                fixed(),
+                {
+                    value: time
+                },
+                'timer'        
+            ]);
         }
-    ]);
+    });
 
-    mario.action(() => {
+    mario.onUpdate(() => {
+        // camPos(mario.pos.x, 180);
         camPos(mario.pos);
     });
 
-    const levelTimerReference = levelTimer(timer) 
-
-    levelTimerReference;
-
-
-
 });
 
-
-go('start', { score: 0, count: 0 });
-
-function levelTimer(timeLeft, timer) {
-    return setInterval(() => {
-        
-        timeLeft--
-        timer.use(text(timeLeft, {size: 64, font: "sinko"}))
-    }, 1000)
-
-}
+go('start');
